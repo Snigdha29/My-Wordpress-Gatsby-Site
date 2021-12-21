8447a56bcc2f64e446538a9f38c6c4c0023c781b
Snigdha29 <snigdha.twilight@gmail.com>
2021-12-21 11:30:10 +0530

    Add featured images

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -87,7 +87,6 @@ export const pageQuery = graphql`
       content
       title
       date(formatString: "MMMM DD, YYYY")
-      }
     }
     previous: wpPost(id: { eq: $previousPostId }) {
       uri
